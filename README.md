7deeb93bc8cf152360766302b9e6fef81c801817
Kevin_Gross <kjg2010@live.com>
2022-04-29 10:14:14 -0600

    Finished Code/README

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # fintech_challenge_18
+This project consists of finishing a blockchain-based ledger system which includes a user-friendly streamlit interface. The focus on this project is to create a ledger which
+facilitates the transfer of money between senders and recievers and to test the validity of the data in the ledger.
+
+---
+## Technologies
+This program requires StreamLit
+The application is a .py file which can be viewed by typing 'streamlit run pychain.py' into your terminal.
+
+Here are the other necessary libraries to import:
+![](./screen_shots/imports.PNG)
+
+
+---
+## Usage
+Once StreamLit is running we can see areas different to input transaction data. We can edit the sender, receiver, and amount. After we set our desired vlaues we can use the
+validate chain button in order to verify our chain's data.
+![](./screen_shots/validate_chain.PNG)
+
+Another feature of our StreamLit page is the block inspector. Found on the left side of the screen, the block inspector allows us to inspect the data of any block on the
+blockchain.
+![](./screen_shots/block_inspector.PNG)
+
+---
+## Contributors
+Kevin Gross
+
+---
+## License
+This program is covered under the MIT license.
--- pychain.py
+++ pychain.py
@@ -49,9 +49,12 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
-
-
+@dataclass
+class Record:
+    sender: str
+    receiver: str
+    amount: float
+    
 ################################################################################
 # Step 2:
 # Modify the Existing Block Data Class to Store Record Data
@@ -68,7 +71,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+    record: Record
 
     creator_id: int
     prev_hash: str = "0"
@@ -81,8 +84,8 @@ class Block:
         record = str(self.record).encode()
         sha.update(record)
 
-        creator_id = str(self.creator_id).encode()
-        sha.update(creator_id)
+        #creator_id = str(self.creator_id).encode()
+        #sha.update(creator_id)
 
         timestamp = str(self.timestamp).encode()
         sha.update(timestamp)
@@ -166,19 +169,19 @@ pychain = setup()
 
 # @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
+#input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender_data = st.text_input("Sender:")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver_data = st.text_input("Receiver:")
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount_data = st.number_input("Amount:")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -189,7 +192,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        record=Record(sender_data, receiver_data, amount_data),
         creator_id=42,
         prev_hash=prev_block_hash
     )
